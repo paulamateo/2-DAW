4a688916fe1eb51d9537a4953c87d5125276e96e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2023-10-18 19:31:11 +0200

    Ejercicios 18/10/2023

--- projects/training-daw-sv/UD3/09_ajax/fetch/main.js
+++ projects/training-daw-sv/UD3/09_ajax/fetch/main.js
@@ -1,9 +1,16 @@
 window.onload = function() {
     console.log('document loaded');
 
-    fetch('http://dummy.restapiexample.com/api/v1/employees')
+    let botonCargarDatos = document.getElementById('botonCargarDatos');
+    let tablaEmpleados = document.getElementById('tablaEmpleados');
+
+    botonCargarDatos.addEventListener('click', function() {
+        fetch('http://dummy.restapiexample.com/api/v1/employees')
         .then(response => response.json())
         .then(response => {
             console.log(response)
         })
+        .catch(console.log("Error " +error))
+    })
+
 }
